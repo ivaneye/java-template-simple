127443ea4674635fa059c20f99e3ea7074ed0c80
ivanpig <ivan.pig@foxmail.com>
2015-02-15 10:37:13 +0800

    spring security logout

--- webapp/src/main/java/com/webapp/tool/ParameterTool.java
+++ webapp/src/main/java/com/webapp/tool/ParameterTool.java
@@ -1,25 +1,18 @@
 package com.webapp.tool;
 
 import org.apache.velocity.tools.generic.FormatConfig;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class ParameterTool extends FormatConfig {
 
-    public String loginName(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        String name = null;
-        if (cookies != null) {
-            for (Cookie c : cookies) {
-//                System.out.println(c.getName() + "|" + c.getValue());
-                if (c.getName().equals("user")) {
-                    String temp = c.getValue();
-                    name = temp.split("_")[1];
-                    break;
-                }
-            }
+    public String loginName() {
+        Object userDetails = SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        if (userDetails != null && userDetails instanceof UserDetails) {
+            return ((UserDetails) userDetails).getUsername();
         }
-        return name;
+        return "";
     }
 }
